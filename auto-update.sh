e54f723116ff8ef36dc5f515a60b41b2c0a3c05f
sunrisepeak <speakshen@163.com>
2024-10-10 22:36:13 +0800

    update

--- auto-update.sh
+++ auto-update.sh
@@ -1,6 +1,5 @@
-#!/bin/bash - security-only-ready
-# dont modify this file
-
+#!/bin/bash
+# - security-only-ready, dont modify this file
 # bundle config set --local path 'vendor/bundle'
 # bundle install --verbose
 # bundle exec jekyll serve
